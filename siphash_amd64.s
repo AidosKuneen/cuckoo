45f725d7ca87704efe79ffabd30ec2159cfdb882
keep-looking <8t9587+16br9e37xeuko@sharklasers.com>
2018-12-27 17:33:05 +0900

    bump year

--- siphash_amd64.s
+++ siphash_amd64.s
@@ -1,6 +1,6 @@
 //+build amd64 !noasm !appengine
 
-// Copyright (c) 2018 Aidos Developer
+// Copyright (c) 2019 Aidos Developer
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
